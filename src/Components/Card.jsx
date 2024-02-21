f04b2e3d451ee59602f1c3f63284af91b651f0de
Ali Aalam <aliaalam.me@gmail.com>
2024-02-21 17:02:07 +0500

    eded

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
 import { ItemTypes } from '../ItemTypes'; // Import your item types
+import { FaRegEyeSlash } from "react-icons/fa";
+import { IoEyeOutline } from "react-icons/io5";
 
 const Card = ({ id, name, description, index, moveCard, img }) => {
     const [{ isDragging }, drag] = useDrag({
@@ -50,53 +52,34 @@ const Card = ({ id, name, description, index, moveCard, img }) => {
     return (
         <div ref={node => drag(drop(node))} style={{ opacity }}>
             <div>
-                <div className='bg-[#333]  flex gap-2 justify-between  items-center md:gap-8 px-2 py-4 min-w-[160px] md:min-w-[232px] cursor-pointer'>
+                <div className='bg-[#333]  flex gap-2 justify-between  items-center md:gap-8 px-2 py-4 min-w-[150px] md:min-w-[262px] cursor-pointer'>
                     <div className='flex  items-center space-x-1 md:space-x-2'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4  md:w-6 md:h-6 text-white cursor-pointer">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
                         </svg>
                         {isOpen ? (
 
+                            <>
+                                <div onClick={toggleIcon}>
+                                    <FaRegEyeSlash className='w-4 h-4  md:w-6 md:h-6 text-white cursor-pointer' />
+
+                                </div>
+
+
+                            </>
 
 
 
 
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="w-4 h-4  md:w-6 md:h-6 cursor-pointer text-white"
-                                onClick={toggleIcon}
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-                                />
-                            </svg>
                         ) : (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="w-4 h-4  md:w-6 md:h-6 text-white cursor-pointer"
-                                onClick={toggleIcon}
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                                />
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                                />
-                            </svg>
+                            <>
+
+                                <div onClick={toggleIcon}>
+                                <IoEyeOutline className='w-4 h-4  md:w-6 md:h-6 text-white cursor-pointer' />
+                                </div>
+
+
+                            </>
                         )}
 
 
@@ -104,7 +87,7 @@ const Card = ({ id, name, description, index, moveCard, img }) => {
                     <div>
                         <p className='text-white text-xs md:text-[16px]'>{name}</p>
                     </div>
-                    <div className='flex justify-end'>
+                    <div className='flex justify-end min-w-4  md:min-w-[32px]'>
                         <div className="cursor-pointer" onClick={toggleGrid}>
                             <p className="text-white text-xs md:text-lg font-bold">{isGrid1 ? '1' : '1²'}</p>
                         </div>

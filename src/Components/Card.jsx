59d247ec4e3d00816a559d029d38aecc2754d4aa
Ali Aalam <aliaalam.me@gmail.com>
2024-02-22 17:00:10 +0500

    vvv

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -33,13 +33,11 @@ const Card = ({ id, name, description, index, moveCard, img }) => {
 
     const opacity = isDragging ? 0.5 : 1;
 
-
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleIcon = () => {
         setIsOpen(!isOpen);
     };
-
 
     const [isGrid1, setIsGrid1] = useState(true);
 
@@ -47,49 +45,34 @@ const Card = ({ id, name, description, index, moveCard, img }) => {
         setIsGrid1(!isGrid1);
     };
 
-
-
     return (
-        <div ref={node => drag(drop(node))} style={{ opacity }}>
+        <div ref={node => drag(drop(node))} style={{ opacity }} className={` ${!isGrid1 ? 'w-full bg-blue-200' : 'w-[250px]'}`}>
             <div>
-                <div className='bg-[#333]  flex gap-2 justify-between  items-center md:gap-8 px-2 py-4 min-w-[150px] md:min-w-[262px] cursor-pointer'>
+                <div className='bg-[#333] backdrop-blur-md border border-[#333] bg-opacity-25 flex gap-2 justify-between items-center md:gap-4 px-2 py-4  cursor-pointer'>
                     <div className='flex  items-center space-x-1 md:space-x-2'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4  md:w-6 md:h-6 text-white cursor-pointer">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4  md:w-6 md:h-6 text-white cursor-pointer">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
                         </svg>
                         {isOpen ? (
-
                             <>
                                 <div onClick={toggleIcon}>
                                     <FaRegEyeSlash className='w-4 h-4  md:w-6 md:h-6 text-white cursor-pointer' />
-
                                 </div>
-
-
                             </>
-
-
-
-
                         ) : (
                             <>
-
                                 <div onClick={toggleIcon}>
-                                <IoEyeOutline className='w-4 h-4  md:w-6 md:h-6 text-white cursor-pointer' />
+                                    <IoEyeOutline className='w-4 h-4  md:w-6 md:h-6 text-white cursor-pointer' />
                                 </div>
-
-
                             </>
                         )}
-
-
                     </div>
                     <div>
                         <p className='text-white text-xs md:text-[16px]'>{name}</p>
                     </div>
                     <div className='flex justify-end min-w-4  md:min-w-[32px]'>
                         <div className="cursor-pointer" onClick={toggleGrid}>
-                            <p className="text-white text-xs md:text-lg font-bold">{isGrid1 ? '1' : '1²'}</p>
+                            <p className="text-white text-xs md:text-lg font-bold">{isGrid1 ? '1²' : '1'}</p>
                         </div>
                     </div>
                 </div>
--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Modal({ children, closeModal, onSave }) {
+function Modal({ children, closeModal, onSave, isGrid1, toggleGrid }) {
     return (
         <div className="fixed inset-0 z-50 overflow-auto bg-gray-800 md:flex items-center justify-center  bg-opacity-75 px-4">
 
-            <div className="bg-[#333] bg-opacity-20 rounded-lg pb-6  max-w-[1200px] h-auto">
+            <div className="bg-[#333] bg-opacity-20 rounded-lg pb-6  max-w-xl h-auto">
 
                 <div className='bg-[#333] p-8 text-white   '>
 
@@ -12,20 +12,22 @@ function Modal({ children, closeModal, onSave }) {
                 </div>
 
                 <div className="mt-4 px-2">
-                    {children}
+                    {React.Children.map(children, child =>
+                        React.cloneElement(child, { isGrid1, toggleGrid })
+                    )}
                 </div>
 
                 <div className="mt-4 flex justify-center gap-3">
                     <button onClick={onSave} className=" flex items-center space-x-2 px-6 py-3 bg-[#333] text-white font-bold rounded-md  ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" />
                         </svg>
 
                         <p>   Save</p>
                     </button>
                     <button onClick={closeModal} className=" flex items-center space-x-2 px-6 py-3 bg-[#333] text-white font-bold rounded-md  ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                         </svg>
 
 
